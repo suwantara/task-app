bafafd86f74429cd7204b663c1ed631feb06fd34
suwantara <suwantaraputra1@gmail.com>
2026-02-08 01:26:29 +0800

    Port

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -32,7 +32,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 void bootstrap();
